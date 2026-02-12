cc813ea6865e1fe4745a6d3f6bbec9cb506577d0
Adam Dilworth <atd59@uakron.edu>
2026-02-12 13:07:18 -0500

    Create simple Machine class with clock

--- RaftAlgorithm/Machine.cs
+++ RaftAlgorithm/Machine.cs
@@ -1,6 +1,42 @@
 // Machine class
-
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 public class Machine
 {
-    
+    private CancellationTokenSource cts;
+    private double time;
+
+    public Machine()
+    {
+        cts = new CancellationTokenSource();
+        time = 0;
+    }
+
+    public async Task startMachine()
+    {
+        try
+        {
+            await Task.Run(() => clock(cts.Token), cts.Token);
+        }
+        catch (OperationCanceledException)
+        {
+                Console.WriteLine("Task was gracefully cancelled.");
+        }
+    }
+
+    public void stopMachine()
+    {
+        cts.Cancel();
+    }
+
+    private async Task clock(CancellationToken token)
+    {
+        while(!token.IsCancellationRequested)
+        {
+            Console.WriteLine("Time: " + time);
+            await Task.Delay(100);
+            time += 0.1;
+        }
+    }
 }
--- RaftAlgorithm/Program.cs
+++ RaftAlgorithm/Program.cs
@@ -1,3 +1,13 @@
 ﻿// Main
 
-Console.WriteLine("Hello, World!");
+// create test machine
+var machine1 = new Machine();
+System.Console.WriteLine("Made machine1");
+_ = machine1.startMachine();
+
+// Run machine for 3 seconds
+await Task.Delay(3000);
+
+// Cancel Machine
+machine1.stopMachine();
+System.Console.WriteLine("Close machine1");
